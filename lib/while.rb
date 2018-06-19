e7440c29e7ba95d40baebbef0c11f552933adf54
nrlsolomon <nrlsolom5@gmail.com>
2018-06-19 19:27:12 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,34 @@
 def using_while
 	levitation_force = 6
 	
-	#your code here
+	
+	
 end
+
+def cupid_shuffle
+  counter = 10
+  
+  until counter == 0 do
+    
+    puts "Wingardium Leviosa"
+  
+    counter -= 1
+
+  end
+
+end 
+
+cupid_shuffle
+
+
+
+
+
+
+
+
+
+
+
 
 
