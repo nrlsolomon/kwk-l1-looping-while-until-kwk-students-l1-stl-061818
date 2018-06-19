0b7266f1ed94ec5a74e0a60ce9dd0e2332ec47eb
nrlsolomon <nrlsolom5@gmail.com>
2018-06-19 19:31:24 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
-def using_while
-	levitation_force = 6
+# def using_while
+# 	levitation_force = 6
 	
 	
 	
-end
+# end
 
 def using_while
  
@@ -14,7 +14,7 @@ def using_while
     
     puts "Wingardium Leviosa"
   
-    levitation_force + 1
+    levitation_force =+ 1
 
   end
 
